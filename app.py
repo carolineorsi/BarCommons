da752895d220f2a1709d6c830f7ed267c92e3d3a
Caroline Orsi <caroline.orsi@gmail.com>
2014-12-25 15:45:31 -0800

    Added app and index.html.

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request, render_template
+
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', "abcdefg")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """ This is the 'cover' page of the site """
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
